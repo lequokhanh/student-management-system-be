71da71979bd4361ac61611b69549eadf5410e1e1
lequokhanh <lequockhanhkt03@gmail.com>
2024-12-03 22:06:38 +0700

    up

--- src/main/java/com/example/smsbe/config/Whitelist.java
+++ src/main/java/com/example/smsbe/config/Whitelist.java
@@ -10,7 +10,7 @@ public class Whitelist {
     private static final Map<String, String> WHITELIST = new HashMap<>();
 
     static {
-        WHITELIST.put("v1/auth/**", "ANY"); // "ANY" to allow all methods
+        WHITELIST.put("/v1/auth/**", "ANY"); // "ANY" to allow all methods
         WHITELIST.put("/api/v1/public/**", "GET"); // Allow only GET method
         WHITELIST.put("/swagger-ui/**", "ANY");
         WHITELIST.put("/v3/api-docs/**", "ANY");
