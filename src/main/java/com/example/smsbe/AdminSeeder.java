430c3abb5a1f980626d5e9715a40b3d1ab6576da
Lê Quốc Khánh <62226493+lequokhanh@users.noreply.github.com>
2024-12-04 15:23:48 +0700

    Create AdminSeeder.java

---
+++ src/main/java/com/example/smsbe/AdminSeeder.java
@@ -0,0 +1,38 @@
+package com.example.smsbe;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.example.smsbe.entity.Manager;
+import com.example.smsbe.repositories.ManagerRepository;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class AdminSeeder implements ApplicationListener<ContextRefreshedEvent> {
+    private final ManagerRepository managerRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void onApplicationEvent(@SuppressWarnings("null") ContextRefreshedEvent contextRefreshedEvent) {
+        this.createSuperAdministrator();
+    }
+
+    private void createSuperAdministrator() {
+        Optional<Manager> optionalUser = managerRepository.findByUsername("admin");
+
+        if (optionalUser.isPresent()) {
+            return;
+        }
+
+        var user = new Manager()
+                .setUsername("admin")
+                .setPassword(passwordEncoder.encode("admin"));
+
+        managerRepository.save(user);
+    }
+}
