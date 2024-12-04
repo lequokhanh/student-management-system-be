3d366c0623f0097e2a2b070ae0328fd3419e92af
Lê Quốc Khánh <62226493+lequokhanh@users.noreply.github.com>
2024-12-04 16:27:08 +0700

    Update AuthController.java

--- src/main/java/com/example/smsbe/controller/AuthController.java
+++ src/main/java/com/example/smsbe/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.example.smsbe.controller;
 
+import com.example.smsbe.request.LoginRequest;
 import com.example.smsbe.response.ResponseWrapper;
 import com.example.smsbe.response.TokenResponse;
 import com.example.smsbe.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +17,10 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseWrapper<TokenResponse> login(String username, String password) {
+    public ResponseWrapper<TokenResponse> login(@RequestBody LoginRequest loginRequest) {
         return new ResponseWrapper<TokenResponse>()
                 .setStatusCode(200)
                 .setMessage("Login successful")
-                .setData(authService.login(username, password));
+                .setData(authService.login(loginRequest.getUsername(), loginRequest.getPassword()));
     }
 }
